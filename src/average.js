0a120ea9d822b279e771f7b62c634ff19d36213d
Aline Valadao <valadao.aline@gmail.com>
2022-06-20 22:27:03 +0200

    Requisitos 1 e 2

--- src/average.js
+++ src/average.js
@@ -11,6 +11,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  let soma = 0;
+  
+  if (arr.length === 0) {
+    return undefined;
+  }
+
+  for (let index = 0; index < arr.length; index += 1) {
+    if (typeof arr[index] !== 'number') {
+      return undefined;
+    }  
+    soma += arr[index];
+  }
+  let resultadoMedia = soma / arr.length;
+
+  return Math.round(resultadoMedia);
+};
 
 module.exports = average;
